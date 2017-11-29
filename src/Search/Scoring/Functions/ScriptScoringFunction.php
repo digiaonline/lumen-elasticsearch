402bfec8a38f8c8d008f53559574957abe671ca9
Hung Neo <hungnq1989@gmail.com>
2017-11-29 16:35:53 +0200

    Support script scoring function

---
+++ src/Search/Scoring/Functions/ScriptScoringFunction.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Nord\Lumen\Elasticsearch\Search\Scoring\Functions;
+
+/**
+ * Class ScriptScoringFunction
+ * @package Nord\Lumen\Elasticsearch\Search\Scoring\Functions
+ */
+class ScriptScoringFunction extends AbstractScoringFunction
+{
+    /**
+     * @var mixed
+     */
+    private $params;
+
+    /**
+     * @var string
+     */
+    private $inline;
+
+    /**
+     * @return mixed
+     */
+    public function getParams()
+    {
+        return $this->params;
+    }
+
+    /**
+     * @param $params
+     *
+     * @return $this
+     */
+    public function setParams($params)
+    {
+        $this->params = $params;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInline()
+    {
+        return $this->inline;
+    }
+
+    /**
+     * @param string $inline
+     *
+     * @return $this
+     */
+    public function setInline($inline)
+    {
+        $this->inline = $inline;
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        $script =  [
+            'inline' => $this->getInline(),
+        ];
+
+        $params = $this->getParams();
+
+        if (!empty($params)) {
+            $script['params'] = $this->getParams();
+        }
+
+        return [
+            'script_score' => [
+                'script' => $script
+            ]
+        ];
+    }
+}
