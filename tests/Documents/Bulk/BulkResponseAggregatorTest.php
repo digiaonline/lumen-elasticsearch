dfc63b0df54833bb3353cdaa91312c73e5bcf8c2
Sam Stenvall <sam.stenvall@digia.com>
2019-12-05 11:47:59 +0200

    Test new edge cases in BulkResponseAggregator

--- tests/Documents/Bulk/BulkResponseAggregatorTest.php
+++ tests/Documents/Bulk/BulkResponseAggregatorTest.php
@@ -36,28 +36,50 @@ class BulkResponseAggregatorTest extends TestCase
 
         $response = [
             'items' => [
+                // An item with an error
                 [
                     'type' => [
                         '_index' => 'foo',
                         '_type'  => 'bar',
                         '_id'    => 'baz',
-                        'error'  =>  [
-                            'type'   => 'err type',
-                            'reason' => 'err reason',
+                        'error'  => [
+                            'type'      => 'err type',
+                            'reason'    => 'err reason',
                             'caused_by' => [
                                 'type'   => 'err cause type',
-                                'reason' => 'err cuase reason'
-                            ]
+                                'reason' => 'err cuase reason',
+                            ],
                         ],
-                    ]
-                ]
-            ]
+                    ],
+                ],
+                // An item without an error
+                [
+                    'type' => [
+                        '_index' => 'foo',
+                        '_type'  => 'bar',
+                        '_id'    => 'baz',
+                    ],
+                ],
+                // An item with an error, but no caused_by
+                [
+                    'type' => [
+                        '_index' => 'foo',
+                        '_type'  => 'bar',
+                        '_id'    => 'baz',
+                        'error'  => [
+                            'type'   => 'err type',
+                            'reason' => 'err reason',
+                        ],
+                    ],
+                ],
+            ],
         ];
 
         $this->aggregator->addResponse($response);
 
+        // Only the item with a full error (including caused_by) should be added
         $this->assertCount(1, $this->aggregator->getErrors());
-        
+
         $this->aggregator->reset();
 
         $this->assertFalse($this->aggregator->hasErrors());
