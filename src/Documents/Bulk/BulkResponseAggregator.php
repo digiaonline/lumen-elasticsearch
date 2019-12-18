digiaonline/lumen-elasticsearch

commit 78b97289b6618981bad01ca35c6d8020be8ba4ad

Don't use array_get(), it's not available in Lumen 6.x
It's in laravel/helpers, but that depends on 5.8+ which we don't do, so we can't use that

src/Documents/Bulk/BulkResponseAggregator.php
@@ -43,26 +43,28 @@ class BulkResponseAggregator
      */
     protected function parseErrors(array $response)
     {
-        $items = array_get($response, 'items', []);
-        foreach ($items as $item) {
-            $item = array_first($item);
+        $items = $response['items'] ?? [];
 
-            if (!array_has($item, 'error')) {
+        foreach ($items as $item) {
+            $item = $item['type'];
+
+            // Ignore items without errors
+            $error = $item['error'] ?? null;
+
+            if ($error === null) {
                 continue;
             }
 
-            $index = array_get($item, '_index');
-            $type = array_get($item, '_type');
-            $id = array_get($item, '_id');
+            // Ignore errors without caused_by
+            $causedBy = $error['caused_by'] ?? null;
 
-            $errorType = array_get($item, 'error.type');
-            $errorReason = array_get($item, 'error.reason');
-
-            $causeType = array_get($item, 'error.caused_by.type');
-            $causeReason = array_get($item, 'error.caused_by.reason');
+            if ($causedBy === null) {
+                continue;
+            }
 
             $this->errors[] = sprintf('Error "%s" reason "%s". Cause "%s" reason "%s". Index "%s", type "%s", id "%s"',
-                $errorType, $errorReason, $causeType, $causeReason, $index, $type, $id);
+                $error['type'], $error['reason'], $causedBy['type'], $causedBy['reason'], $item['_index'],
+                $item['_type'], $item['_id']);
         }
     }
 

commit 9b5ce74326fa4428a39f0276109d42875b04b063

Fix a regression from 78b97289b6618981bad01ca35c6d8020be8ba4ad
Fixes "undefined index type" during normal bulk indexing operations. Fixes #138.

src/Documents/Bulk/BulkResponseAggregator.php
@@ -1,5 +1,7 @@
 <?php namespace Nord\Lumen\Elasticsearch\Documents\Bulk;
 
+use Illuminate\Support\Arr;
+
 class BulkResponseAggregator
 {
 
@@ -54,10 +56,10 @@ class BulkResponseAggregator
         $items = $response['items'] ?? [];
 
         foreach ($items as $item) {
-            $item = $item['type'];
+            $operation = Arr::first($item);
 
             // Ignore items without errors
-            $error = $item['error'] ?? null;
+            $error = $operation['error'] ?? null;
 
             if ($error === null) {
                 continue;
@@ -71,8 +73,8 @@ class BulkResponseAggregator
             }
 
             $this->errors[] = sprintf('Error "%s" reason "%s". Cause "%s" reason "%s". Index "%s", type "%s", id "%s"',
-                $error['type'], $error['reason'], $causedBy['type'], $causedBy['reason'], $item['_index'],
-                $item['_type'], $item['_id']);
+                $error['type'], $error['reason'], $causedBy['type'], $causedBy['reason'], $operation['_index'],
+                $operation['_type'], $operation['_id']);
         }
     }
 }

tests/Documents/Bulk/BulkResponseAggregatorTest.php
@@ -36,9 +36,9 @@ class BulkResponseAggregatorTest extends TestCase
 
         $response = [
             'items' => [
-                // An item with an error
+                // An index operation with an error
                 [
-                    'type' => [
+                    'index' => [
                         '_index' => 'foo',
                         '_type'  => 'bar',
                         '_id'    => 'baz',
@@ -52,17 +52,17 @@ class BulkResponseAggregatorTest extends TestCase
                         ],
                     ],
                 ],
-                // An item without an error
+                // A delete operation without an error
                 [
-                    'type' => [
+                    'delete' => [
                         '_index' => 'foo',
                         '_type'  => 'bar',
                         '_id'    => 'baz',
                     ],
                 ],
-                // An item with an error, but no caused_by
+                // A create operation with an error, but no caused_by
                 [
-                    'type' => [
+                    'create' => [
                         '_index' => 'foo',
                         '_type'  => 'bar',
                         '_id'    => 'baz',